7de878af344bddd279c396634e88bf50edc3a5c6
cieplik <dcieplin@gmail.com>
2010-09-12 12:34:40 +0200

    wywalone xbow-base

--- code/xbow-base/src/main/c/flowadm/jconsole.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-jconsole -J-Djava.class.path=xbow-base-1.0-SNAPSHOT-jar-with-dependencies.jar:/usr/java/lib/jconsole.jar:/usr/java/lib/tools.jar
-
--- code/xbow-base/src/main/c/flowadm/jtest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
-java -cp xbow-base-1.0-SNAPSHOT-jar-with-dependencies.jar agh.msc.xbowbase.App
