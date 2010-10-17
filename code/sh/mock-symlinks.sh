badf28f000b71aa03f668e696c8d75f8b5aef8f2
cieplik <dcieplin@gmail.com>
2010-10-17 18:35:51 +0200

    symlinki - poprawka

--- code/sh/mock-symlinks.sh
+++ code/sh/mock-symlinks.sh
@@ -5,7 +5,7 @@ for LIB in etherstub flow link; do
 echo $LIB
 
 	ln -sn ../../../../../xbow-native-mock/src/main/c/mock xbow/xbow-native/xbow-native-lib/xbow-native-lib-$LIB/src/test/c/mock
-	ln -sn ../../../../../xbow-native-mock/src/main/c/mock xbow/xbow-native/xbow-native-lib/xbow-native-lib-$LIB/src/test/include/mock
+	ln -sn ../../../../../xbow-native-mock/src/main/include/mock xbow/xbow-native/xbow-native-lib/xbow-native-lib-$LIB/src/test/include/mock
 
 done
 
