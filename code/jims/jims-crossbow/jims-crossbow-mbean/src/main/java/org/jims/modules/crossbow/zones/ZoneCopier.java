23884d06ee9a2c6665704cc9b9b393fba0f12a04
robert <robert@opensolaris.(none)>
2011-01-10 14:57:04 +0100

    Poprawa zonecopiera

--- code/jims/jims-crossbow/jims-crossbow-mbean/src/main/java/org/jims/modules/crossbow/zones/ZoneCopier.java
+++ code/jims/jims-crossbow/jims-crossbow-mbean/src/main/java/org/jims/modules/crossbow/zones/ZoneCopier.java
@@ -23,12 +23,11 @@ public class ZoneCopier implements ZoneCopierMBean {
             String cmd[] = {
                 "bash",
                 "-c",
-                "cp " + fromLocation + " " + toLocation};
+                "cp -R " + fromLocation + " " + toLocation};
             proc = rt.exec(cmd);
 
-            Scanner outputScanner = new Scanner(proc.getInputStream());
+            proc.waitFor();
             result = (proc.exitValue() == 0);
-            outputScanner.close();
         } catch (Exception ex) {
             logger.error("Exception while copying zone has occured: " + ex.getMessage());
         }
