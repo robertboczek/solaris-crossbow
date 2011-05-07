5d62592f10d2913c5f2fa4582657daae15e1433d
cieplik <dcieplin@gmail.com>
2011-05-07 12:45:43 +0200

    supervisor: utest

--- code/jims/jims-crossbow/jims-crossbow-infrastructure/src/test/java/org/jims/modules/crossbow/infrastructure/supervisor/MultipleWorkersSupervisorTest.java
+++ code/jims/jims-crossbow/jims-crossbow-infrastructure/src/test/java/org/jims/modules/crossbow/infrastructure/supervisor/MultipleWorkersSupervisorTest.java
@@ -1,5 +1,8 @@
 package org.jims.modules.crossbow.infrastructure.supervisor;
 
+import java.util.Map;
+import java.util.Collection;
+import java.util.HashMap;
 import org.jims.modules.crossbow.objectmodel.resources.Endpoint;
 import org.jims.modules.crossbow.objectmodel.resources.Switch;
 import java.util.LinkedList;
@@ -9,6 +12,8 @@ import org.jims.modules.crossbow.objectmodel.Actions;
 import org.jims.modules.crossbow.infrastructure.helper.model.ModelHelper;
 import org.jims.modules.crossbow.objectmodel.Assignments;
 import org.jims.modules.crossbow.objectmodel.ObjectModel;
+import org.jims.modules.crossbow.objectmodel.VlanInterfaceAssignment;
+import org.jims.modules.crossbow.objectmodel.resources.Interface;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -96,6 +101,36 @@ public class MultipleWorkersSupervisorTest {
 			assert ( null == firstActionsCaptor.getValue().get( o ) );
 			assert ( null != secondActionsCaptor.getValue().get( o ) );
 		}
+
+	}
+
+
+	@Test
+	public void testAnnotationsApplied() {
+
+		Collection< Map< Interface, String > > parts = new LinkedList< Map< Interface, String > >();
+
+		Interface i0 = new Interface( "my-int", "my-pro" );
+		Interface i1 = new Interface( "my-int2", "my-pro" );
+
+		Map< Interface, String > ifaces = new HashMap< Interface, String >();
+		ifaces.put( i0, W0 );
+		ifaces.put( i1, W1 );
+
+		parts.add( ifaces );
+
+		Assignments assignments = new Assignments();
+
+		assert ( null == assignments.getAnnotation( i0 ) );
+		assert ( null == assignments.getAnnotation( i1 ) );
+
+		assignments.putAnnotation( i0, new VlanInterfaceAssignment( 13 ) );
+
+		assert ( null != assignments.getAnnotation( i0 ) );
+
+		supervisor.createVlanAssignments( parts, null, assignments );
+
+		assert ( null != assignments.getAnnotation( i1 ) );
 
 	}
 
