a9ba5e8c3eb54a5a76b35bf86b66270cb074c874
Super-User <root@opensolaris.(none)>
2011-05-10 18:53:21 +0200

    Watek dla timer taska

--- code/jims/jims-crossbow/jims-crossbow-mbean/src/main/java/org/jims/modules/crossbow/link/LinkStatisticsGatherer.java
+++ code/jims/jims-crossbow/jims-crossbow-mbean/src/main/java/org/jims/modules/crossbow/link/LinkStatisticsGatherer.java
@@ -22,7 +22,69 @@ public class LinkStatisticsGatherer {
     private LinkHelper linkHelper;
     private Timer timer = new Timer();
     private final String linkName;
+
     private static final Logger logger = Logger.getLogger(LinkStatisticsGatherer.class);
+    
+    private Thread timerThread = new Thread() {
+
+        public void run() {
+
+            Map<LinkStatistics, Long> map = getEmtpyMap();
+
+            final LinkHelper helper = linkHelper;
+
+            for (int i = 0; i < 10; i++) {
+                minuteValueList.add(map);
+                fiveMinutesValueList.add(map);
+                hourValueList.add(map);
+                dayValueList.add(map);
+            }
+
+            timer.schedule(new TimerTask() {
+
+                @Override
+                public void run() {
+
+                    updateStatistics(minuteValueList, helper);
+                    logger.trace("Minute statistics for etherstub " + linkName + " updated");
+
+                }
+            }, 0, 6000);//zawiera 10 wartosci
+
+            timer.schedule(new TimerTask() {
+
+                @Override
+                public void run() {
+                    updateStatistics(fiveMinutesValueList, helper);
+                    logger.trace("Five-minute statistics for etherstub " + linkName + " updated");
+                }
+            }, 0, 3000);//zawiera 10 wartosci
+
+            timer.schedule(new TimerTask() {
+
+                @Override
+                public void run() {
+                    updateStatistics(hourValueList, helper);
+                    logger.trace("Hourly statistics for etherstub " + linkName + " updated");
+                }
+            }, 0, 36000);//zawiera 10 wartosci
+
+            timer.schedule(new TimerTask() {
+
+                @Override
+                public void run() {
+                    updateStatistics(dayValueList, helper);
+                    logger.trace("Daily statistics for etherstub " + linkName + " updated");
+                }
+            }, 0, 864000);//zawiera 10 wartosci
+
+            try {
+                Thread.sleep(Integer.MAX_VALUE);
+            } catch (InterruptedException ex) {
+            }
+
+        }
+    };
 
     public LinkStatisticsGatherer(String linkName) {
         this.linkName = linkName;
@@ -31,10 +93,10 @@ public class LinkStatisticsGatherer {
     void setLinkHelper(LinkHelper linkHelper) {
         this.linkHelper = linkHelper;
     }
-    private LinkedList<Map<LinkStatistics, Long>> minuteValueList = new LinkedList<Map<LinkStatistics, Long>>();
-    private LinkedList<Map<LinkStatistics, Long>> fiveMinutesValueList = new LinkedList<Map<LinkStatistics, Long>>();
-    private LinkedList<Map<LinkStatistics, Long>> hourValueList = new LinkedList<Map<LinkStatistics, Long>>();
-    private LinkedList<Map<LinkStatistics, Long>> dayValueList = new LinkedList<Map<LinkStatistics, Long>>();
+    private final LinkedList<Map<LinkStatistics, Long>> minuteValueList = new LinkedList<Map<LinkStatistics, Long>>();
+    private final LinkedList<Map<LinkStatistics, Long>> fiveMinutesValueList = new LinkedList<Map<LinkStatistics, Long>>();
+    private final LinkedList<Map<LinkStatistics, Long>> hourValueList = new LinkedList<Map<LinkStatistics, Long>>();
+    private final LinkedList<Map<LinkStatistics, Long>> dayValueList = new LinkedList<Map<LinkStatistics, Long>>();
 
     private Map<LinkStatistics, Long> getEmtpyMap() {
         Map<LinkStatistics, Long> map = new HashMap<LinkStatistics, Long>();
@@ -42,59 +104,6 @@ public class LinkStatisticsGatherer {
             map.put(linkStatistics, 0L);
         }
         return map;
-    }
-
-    private void initContent() {
-
-        Map<LinkStatistics, Long> map = getEmtpyMap();
-
-        final LinkHelper helper = linkHelper;
-
-        for (int i = 0; i < 10; i++) {
-            minuteValueList.add(map);
-            fiveMinutesValueList.add(map);
-            hourValueList.add(map);
-            dayValueList.add(map);
-        }
-
-        timer.schedule(new TimerTask() {
-
-            @Override
-            public void run() {
-
-                updateStatistics(minuteValueList, helper);
-                logger.trace("Minute statistics for etherstub " + linkName + " updated");
-
-            }
-        }, 0, 6000);//zawiera 10 wartosci
-
-        timer.schedule(new TimerTask() {
-
-            @Override
-            public void run() {
-                updateStatistics(fiveMinutesValueList, helper);
-                logger.trace("Five-minute statistics for etherstub " + linkName + " updated");
-            }
-        }, 0, 3000);//zawiera 10 wartosci
-
-        timer.schedule(new TimerTask() {
-
-            @Override
-            public void run() {
-                updateStatistics(hourValueList, helper);
-                logger.trace("Hourly statistics for etherstub " + linkName + " updated");
-            }
-        }, 0, 36000);//zawiera 10 wartosci
-
-        timer.schedule(new TimerTask() {
-
-            @Override
-            public void run() {
-                updateStatistics(dayValueList, helper);
-                logger.trace("Daily statistics for etherstub " + linkName + " updated");
-            }
-        }, 0, 864000);//zawiera 10 wartosci
-
     }
 
     private void updateStatistics(LinkedList<Map<LinkStatistics, Long>> valueList, final LinkHelper helper) {
@@ -125,7 +134,7 @@ public class LinkStatisticsGatherer {
     }
 
     public void start() {
-        initContent();
+        timerThread.start();
     }
 
     public void stop() {
